6befafc02271bb6df509875d05b4ec8e4f62b987
Emil Gräs <emilgras@Emils-MacBook-Pro.local>
2017-02-15 14:31:43 +0100

    Initial Commit

---
+++ BankAccountExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankAccountExercise
+//
+//  Created by Emil Gräs on 15/02/2017.
+//  Copyright © 2017 Emil Gräs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
